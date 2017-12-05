dd3200998110f072daddc85aa35cacc53ce47848
Francis <tvz0fc00@gmail.com>
2017-12-05 09:38:32 +0800

    index file modified

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $scramble_arr = [5,3,1,4,2];
+
+        for($x = 0; $x < count($scramble_arr); $x++) {
+            for($y = 0; $y < count($scramble_arr); $y++) {
+                $numx = 0;
+                if($scramble_arr[$x] < $scramble_arr[$y]) {
+                    $numx = $scramble_arr[$x];
+                    $scramble_arr[$x] = $scramble_arr[$y];
+                    $scramble_arr[$y] = $numx;
+                }
+            }
+        }
+        
+    ?>
+</body>
+</html>
